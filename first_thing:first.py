3021dd90a57b23b79025af1bae39c1e8daba45da
kky17 <ahmetselim2117@gmail.com>
2022-07-30 18:16:22 +0200

    Create first_thing:first.py

---
+++ first_thing:first.py
@@ -0,0 +1,7 @@
+#This is my Phyton journey with a bootcamp to get a valuable skill which I will later on add on to. 
+#I will be learning Phyton as well as other Programming languages to become the high skilled develoer I plan on bieng before starting my own start-up.
+
+Ahmet_will_suceed = True
+
+if Ahmet_will_suceed:
+    print("You gotta love the process")
